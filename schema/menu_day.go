515aeffe9767fb1799647cdd126be3e987173b34
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-31 15:11:52 -0400

    Create schema for menu info

--- schema/menu_day.go
+++ schema/menu_day.go
@@ -3,6 +3,7 @@ package schema
 import "time"
 
 type MenuDay struct {
-	Date                time.Time `json:"date"`
-	HasUnpublishedMenus bool `json:"has_unpublished_menus"`
+	Date                time.Time                `json:"date"`
+	HasUnpublishedMenus bool                     `json:"has_unpublished_menus"`
+	MenuInfo            map[string]MenuInfoValue `json:"menu_info"`
 }
