00358e4e15a374f24f27bf88624f6fe2c5bae437
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-31 16:32:50 -0400

    Don't parse some dates in returned JSON data

--- schema/menu_day.go
+++ schema/menu_day.go
@@ -1,9 +1,7 @@
 package schema
 
-import "time"
-
 type MenuDay struct {
-	Date                time.Time                `json:"date"`
+	Date                string                `json:"date"`
 	HasUnpublishedMenus bool                     `json:"has_unpublished_menus"`
 	MenuInfo            map[string]MenuInfoValue `json:"menu_info"`
 	MenuItems           []MenuItem               `json:"menu_items"`
--- schema/menu_week.go
+++ schema/menu_week.go
@@ -1,12 +1,10 @@
 package schema
 
-import "time"
-
 type MenuWeek struct {
-	StartDate time.Time `json:"start_date"`
+	StartDate string `json:"start_date"`
 	MenuTypeID int `json:"menu_type_id"`
 	ID int `json:"id"`
-	LastUpdated time.Time `json:"last_updated"`
+	LastUpdated string `json:"last_updated"`
 	BoldAllEntreesEnabled bool `json:"bold_all_entrees_enabled"`
 	Published bool `json:"published"`
 	DisplayName string `json:"display_name"`
