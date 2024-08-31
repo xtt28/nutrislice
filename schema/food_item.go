c12bbbfee4c88720b7ff6bfa804833d7a6a1dc6a
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-31 15:56:31 -0400

    Create struct FoodItemNutritionInfo

--- schema/food_item.go
+++ schema/food_item.go
@@ -13,7 +13,7 @@ type FoodItem struct {
 	FoodHighlightMessage string `json:"food_highlight_message"`
 	FileURL string `json:"file_url"`
 	DownloadLabel string `json:"download_label"`
-	// RoundedNutritionInfo FoodItemNutritionInfo `json:"rounded_nutrition_info"`
+	RoundedNutritionInfo FoodItemNutritionInfo `json:"rounded_nutrition_info"`
 	ServingSizeInfo FoodItemServingSizeInfo `json:"serving_size_info"`
 	HasNutritionInfo bool `json:"has_nutrition_info"`
 	// Icons FoodItemIconData `json:"icons"`
@@ -37,4 +37,27 @@ type FoodItem struct {
 type FoodItemServingSizeInfo struct {
 	ServingSizeAmount string `json:"serving_size_amount"`
 	ServingSizeUnit string `json:"serving_size_unit"`
+}
+
+type FoodItemNutritionInfo struct {
+	Calories       float64 `json:"calories"`
+	Fat            float64 `json:"g_fat"`
+	SaturatedFat   float64 `json:"g_saturated_fat"`
+	TransFat       float64 `json:"g_trans_fat"`
+	Cholesterol    float64 `json:"mg_cholesterol"`
+	Carbs          float64 `json:"g_carbs"`
+	AddedSugar     float64 `json:"g_added_sugar"`
+	Sugar          float64 `json:"g_sugar"`
+	Potassium      float64 `json:"mg_potassium"`
+	Sodium         float64 `json:"mg_sodium"`
+	Fiber          float64 `json:"g_fiber"`
+	Protein        float64 `json:"g_protein"`
+	Iron           float64 `json:"mg_iron"`
+	Calcium        float64 `json:"mg_calcium"`
+	VitaminC       float64 `json:"mg_vitamin_c"`
+	VitaminA       float64 `json:"iu_vitamin_a"`
+	RetinolEquivalents float64 `json:"re_vitamin_a"`
+	MicrogramsVitaminA float64 `json:"mcg_vitamin_a"`
+	VitaminD       float64 `json:"mg_vitamin_d"`
+	MicrogramsVitaminD float64 `json:"mcg_vitamin_d"`
 }
