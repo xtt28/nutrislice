760d4415b793c9de8de50381c3d2ca971f338a1e
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-08-31 15:30:10 -0400

    Create MenuItem schema

--- schema/menu_day.go
+++ schema/menu_day.go
@@ -6,4 +6,5 @@ type MenuDay struct {
 	Date                time.Time                `json:"date"`
 	HasUnpublishedMenus bool                     `json:"has_unpublished_menus"`
 	MenuInfo            map[string]MenuInfoValue `json:"menu_info"`
+	MenuItems           []MenuItem               `json:"menu_items"`
 }
